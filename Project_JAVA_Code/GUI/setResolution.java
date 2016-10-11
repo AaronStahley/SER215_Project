91a709d3cf01135ad91a19fa0f7cf724190fa5e7
Aaron Stahley <acstahle@asu.edu>
2016-10-11 01:08:12 -0700

    deleted setResolution file added a menuBar class to add to panel and set up all the resolutions in menuBar

--- Project_JAVA_Code/GUI/setResolution.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-
-public class setResolution {
-	
-	// This will be a class that lists all the resolutions 
-	// you can pick from. It will simply set the JFrame size
-	// To the resolution the user picks.. should be simple enough. 
-
-}
--- Project_JAVA_Code/main/Main.java
+++ Project_JAVA_Code/main/Main.java
@@ -3,9 +3,13 @@ package main;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import javax.swing.JFrame;
+
+import GUI.menuBar;
 import GUI.startPanel;
 
 public class Main {
+	
+	public static JFrame frame;
 	
 	public static void main (String[] args){
 		
@@ -17,11 +21,19 @@ public class Main {
 		startPanel panelStart = new startPanel(); 
 		
 		// Contains all the JFrame attributes. 
+		menuBar mb = new menuBar(); 
+		frame = new JFrame("Game"); 
 		
-		JFrame frame = new JFrame("Game"); 
 		frame.setVisible(true);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//-------- Adding components to the JFrame ------------//
+		
+		//Adds the panel to the frame
 		frame.getContentPane().add(panelStart);
+		// Adds the menuBar to the frame
+		frame.getContentPane().add(mb);
 		
 		// Sets the panel size to the resolution of the monitor
 		frame.setSize(gd.getDisplayMode().getWidth(),gd.getDisplayMode().getHeight());
