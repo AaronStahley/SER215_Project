be8c1d0bfcaf6cedcc8d5d7a27eba59c621f055b
Aaron Stahley <acstahle@asu.edu>
2016-10-15 23:26:42 -0700

    added the main game board

---
+++ Project_JAVA_Code/GUI/gameBoardPanel.java
@@ -0,0 +1,39 @@
+package GUI;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class gameBoardPanel extends JPanel{
+	
+	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	
+	private GridBagConstraints gbc = new GridBagConstraints(); 
+	private ImageIcon bgImage = new ImageIcon(this.getClass().getResource("mancala_GB.jpg"));
+	
+	private JLabel bgLabel; 
+	
+	public gameBoardPanel() throws MalformedURLException, IOException{
+		
+		setLayout(new GridBagLayout());
+		
+		
+		Image scaledImage = bgImage.getImage().getScaledInstance(gd.getDisplayMode().getWidth(),gd.getDisplayMode().getHeight(), Image.SCALE_DEFAULT);
+
+		bgImage.setImage(scaledImage);
+		gbc.gridx = 0; 
+		gbc.gridy = 0; 
+		bgLabel = new JLabel(bgImage); 
+		add(bgLabel,gbc); 
+		
+	}
+
+}
