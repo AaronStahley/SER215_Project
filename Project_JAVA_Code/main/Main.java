be12d116c662f42202fc818625326860320c290f
Aaron Stahley <acstahle@asu.edu>
2016-10-17 18:23:11 -0700

    Added the menu bar to the JFrame

--- Project_JAVA_Code/main/Main.java
+++ Project_JAVA_Code/main/Main.java
@@ -8,26 +8,33 @@ import java.net.MalformedURLException;
 import javax.swing.JFrame;
 
 import GUI.gameBoardPanel;
+import GUI.menuBar;
+import GUI.startPanel;
 //import GUI.menuBar;
 //import GUI.startPanel;
 
 public class Main {
 	
 	public static JFrame frame;
+	public static gameBoardPanel gbp;
+	public static startPanel panelStart;
+	public static menuBar mb; 
 	
 	public static void main (String[] args) throws MalformedURLException, IOException{
 		
 		// ------------------------ GUI STUFF ------------------------------//
 	
-		// Needed to detect the screen resolution 
+		// Needed to detect the screen resolution sizes the window to your monitor resolution. 
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-
-		//startPanel panelStart = new startPanel(); 
 		
 		// Contains all the JFrame attributes. 
-		//menuBar mb = new menuBar();
-		gameBoardPanel gbp = new gameBoardPanel(); 
+		mb = new menuBar(); 
+		panelStart = new startPanel();
+		gbp = new gameBoardPanel(); 		
+		
 		frame = new JFrame("Mancala"); 
+		
+		frame.pack();
 		frame.setVisible(true);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -35,30 +42,10 @@ public class Main {
 		//-------- Adding components to the JFrame ------------//
 		
 		// Adds the menuBar to the frame
-		
-		
-		
-		
-		
-		/////////------TO DO------//////////////
-		
-		//frame.getContentPane().add(mb);
-		
-		/* This needs to be redone using a JMenubar class and 
-		 * not my own class. 
-		 */
-		
-		/////////--------------//////////////
-		
-		
-		
-		
+		frame.setJMenuBar(mb.createMenuBar());
+				
 		//Adds the startPanel to the frame
-			//frame.getContentPane().add(panelStart);
-	
-		//Adds the gameBoardPanel to the frame
-		frame.getContentPane().add(gbp);
-		
+		frame.getContentPane().add(panelStart);		
 		
 		// Sets the panel size to the resolution of the monitor
 		frame.setSize(gd.getDisplayMode().getWidth(),gd.getDisplayMode().getHeight());
