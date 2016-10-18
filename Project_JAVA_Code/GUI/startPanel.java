e1b8300c2860dcd5c96b5f2eb132a8da69711348
Aaron Stahley <acstahle@asu.edu>
2016-10-17 18:17:50 -0700

    Made a start button and had it switch the panels so one is deleted from the frame and the other is added.

--- Project_JAVA_Code/GUI/startPanel.java
+++ Project_JAVA_Code/GUI/startPanel.java
@@ -2,21 +2,44 @@ package GUI;
 
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import main.Main;
 
 // Contains all the content for the start Screen. 
 
 public class startPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
-
+	
+	private JButton startButton;
+	
 	GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	
+	
 	
 	public startPanel(){
 				
 		setLayout(null); 
+		
+		startButton = new JButton("Start"); 
+		startButton.setBounds(700, 50, 100, 50);
+		add(startButton);
+		
+		//This provides the action for the start button. 
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+			
+			// Removes the startFrame and replaces it to the Game board. 
+			Main.frame.getContentPane().removeAll();
+			Main.frame.getContentPane().add(Main.gbp);
+			Main.frame.revalidate(); // refreshes the JFrame. 
+		  }
+		});
 		
 	}	
 }
